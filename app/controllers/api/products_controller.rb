d42111a27c8413b6f3a69e01d91d49f04d5de861
birdthechicken <sinangundogar61@gmail.com>
2019-05-02 10:05:26 -0500

    updated route for displaying

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,5 +1,11 @@
 class Api::ProductsController < ApplicationController
   def all_products_action
-    @contacts = Product.all
+    @products = Product.all
     render 'all_products_view.json.jbuilder'
+  end
+
+  def one_product_action
+    @product = Product.first
+    render 'one_product_view.json.jbuilder'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     get '/all_products_url' => 'products#all_products_action'
+    get '/one_product_url' => 'products#one_product_action'
     
   end
 end
