cba3522390bae89ae4d54394fd41f34b4438fa28
birdthechicken <sinangundogar61@gmail.com>
2019-05-07 16:28:34 -0500

    week3 day2

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,18 @@
 class Product < ApplicationRecord
+  def is_discounted?
+    price < 1000
+    # if price < 1000
+    #    true
+    # else
+    #    false
+    # end
+  end
+
+  def tax
+    price * 0.09 
+  end
+
+  def total
+    price + tax
+  end
 end
