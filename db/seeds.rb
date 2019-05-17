adba21bb555c685ed16f0910e81264b6fd325857
birdthechicken <sinangundogar61@gmail.com>
2019-05-17 17:04:50 -0500

    week4 day4

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,4 +24,4 @@
 # end
 
 
- Product.all.each { |product| Image.create(url: product.image_url, product_id: product.id) }
+ # Product.all.each { |product| Image.create(url: product.image_url, product_id: product.id) }
